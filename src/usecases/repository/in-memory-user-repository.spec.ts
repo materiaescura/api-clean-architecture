3183b68b5b1f1aae38f1b0a074c8353d429dcc7a
Diego Rabelo <rabelo.github@gmail.com>
2023-11-17 20:46:56 -0300

    test: add and find user and check its existence

--- src/usecases/repository/in-memory-user-repository.spec.ts
+++ src/usecases/repository/in-memory-user-repository.spec.ts
@@ -1,5 +1,5 @@
-import { UserData } from "../user-data"
-import { InMemoryUserRepository } from "./in-memory-user-repository"
+import { UserData } from '../user-data'
+import { InMemoryUserRepository } from './in-memory-user-repository'
 
 describe('In memory user repository', () => {
   it('should return null if user is not found', async () => {
@@ -7,5 +7,15 @@ describe('In memory user repository', () => {
     const userRepository = new InMemoryUserRepository(users)
     const user = await userRepository.findUserByEmail('any@email.com')
     expect(user).toBeNull()
+  })
+
+  it('should return user if it is found in repository', async () => {
+    const users: UserData[] = []
+    const name = 'any_name'
+    const email = 'any@email.com'
+    const userRepository = new InMemoryUserRepository(users)
+    await userRepository.add({name, email})
+    const user = await userRepository.findUserByEmail(email)
+    expect(user?.name).toBe(name)
   })
 })
