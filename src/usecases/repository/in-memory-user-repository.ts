57a25f7687d10824d9ca8f5a405c11355b1529de
Diego Rabelo <rabelo.github@gmail.com>
2023-11-18 08:50:06 -0300

    feat: return all users in the repository

--- src/usecases/repository/in-memory-user-repository.ts
+++ src/usecases/repository/in-memory-user-repository.ts
@@ -2,27 +2,26 @@ import { UserRepository } from '../ports/user-data'
 import { UserData } from '../user-data'
 
 export class InMemoryUserRepository implements UserRepository {
-    constructor(private repository: UserData[]) {
-        this.repository = repository
-    }
+  constructor(private repository: UserData[]) {
+    this.repository = repository
+  }
 
-    public async add (user: UserData): Promise<void> {
-        const userExists = await this.exists(user)
-        if(!userExists) this.repository.push(user) 
-    }
+  public async add(user: UserData): Promise<void> {
+    const userExists = await this.exists(user)
+    if (!userExists) this.repository.push(user)
+  }
 
-    public async findUserByEmail (email: string): Promise<UserData  | null> {
-        const users = this.repository.filter(user => user.email === email)
-        if(users.length > 0) return users[0]
-        return null
-    }
+  public async findUserByEmail(email: string): Promise<UserData | null> {
+    const users = this.repository.filter((user) => user.email === email)
+    if (users.length > 0) return users[0]
+    return null
+  }
 
-    public async findAllUsers (): Promise<UserData[]> {
-        throw new Error('Method not implemented.')
-    }
-    
-    public async exists (user: UserData): Promise<boolean> {
-        return this.repository.some(el => el.email === user.email)
-    }
-  
+  public async findAllUsers(): Promise<UserData[]> {
+    return this.repository
+  }
+
+  public async exists(user: UserData): Promise<boolean> {
+    return this.repository.some((el) => el.email === user.email)
+  }
 }
