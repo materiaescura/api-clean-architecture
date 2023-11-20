e78b84482f289edb7c7dacf3d16a783048141efb
Diego Rabelo <rabelo.github@gmail.com>
2023-11-20 06:31:49 -0300

    feat: validate empty local part and domain

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -6,8 +6,8 @@ export class Email {
 
     const [local, domain] = email.split('@')
 
-    if (local.length > 64) return false
-    if (domain.length > 255) return false
+    if (local.length > 64 || local.length === 0) return false
+    if (domain.length > 255 || domain.length === 0) return false
 
     return true
   }
