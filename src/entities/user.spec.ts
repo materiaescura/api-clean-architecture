9384d9d2f5f581a599f6405376964e05444802d7
Diego Rabelo <rabelo.github@gmail.com>
2023-11-23 10:21:33 -0300

    test: verify name while creating user

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -1,5 +1,6 @@
 import { left, right } from '../shared/either'
 import { InvalidEmailError } from './errors/invalid-email-error'
+import { InvalidNameError } from './errors/invalid-name-error'
 import { User } from './user'
 
 describe('User domain entity', () => {
@@ -7,5 +8,17 @@ describe('User domain entity', () => {
     const invalidEmail = 'invalid_email'
     const error = User.create({ name: 'any_name', email: invalidEmail })
     expect(error).toEqual(left(new InvalidEmailError()))
+  })
+
+  it('should not create user with invalid name {too few characters}', () => {
+    const invalidName = 'O  '
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
+  })
+
+  it('should not create user with invalid name {too many characters}', () => {
+    const invalidName = 'O'.repeat(257)
+    const error = User.create({ name: invalidName, email: 'any@mail.com' })
+    expect(error).toEqual(left(new InvalidNameError()))
   })
 })
