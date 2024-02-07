b49560c83cb465788a746e2dbc54e31bc4d2851f
Diego Rabelo <rabelo.github@gmail.com>
2024-02-07 10:10:58 -0300

    feat: apply cors middleware

--- src/main/config/middleware.ts
+++ src/main/config/middleware.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '@/main/middleware/body-parser'
+import { cors } from '@/main/middleware/cors'
 
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
