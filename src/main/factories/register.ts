c19c86309704256a6ef0b9ad1bb66dd2aad871aa
Diego Rabelo <rabelo.github@gmail.com>
2024-02-16 21:52:32 -0300

    refactor: move in memory repository to source folder

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,6 +1,6 @@
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
 import { RegisterUserController } from '@/web-controllers/register-user-controller'
-import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 
 export const makeUserRegisterUserController = (): RegisterUserController => {
   const inMemoryUserRepository = new InMemoryUserRepository([])
--- test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
+++ test/usecases/register-user-on-mailing-list/register-user-on-mailing-list.spec.ts
@@ -1,7 +1,7 @@
 import type { UserData } from '@/entities'
 import { UserRepository } from '@/usecases/register-user-on-mailing-list/ports'
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
-import { InMemoryUserRepository } from '@test/usecases/register-user-on-mailing-list/repository'
+import { InMemoryUserRepository } from '@/usecases/register-user-on-mailing-list/repository'
 
 describe('Register user on mailing list use case', () => {
   it('should add user with complete data to mailing list', async () => {
