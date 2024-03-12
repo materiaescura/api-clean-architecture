d96d64207103f94af76907f5641e73d19e203975
Diego Rabelo <rabelo.github@gmail.com>
2024-03-12 10:25:12 -0300

    feat: register and send email

---
+++ src/usecases/register-and-send-email/register-and-send-email.ts
@@ -0,0 +1,41 @@
+import { InvalidEmailError, InvalidNameError } from '@/entities/errors'
+import { UseCase } from '../ports'
+import { RegisterUserOnMailingList } from '../register-user-on-mailing-list'
+import { SendEmail } from '../send-email/send-email'
+import { User, UserData } from '@/entities'
+import { Either, left, right } from '@/shared'
+import { MailServiceError } from '../errors/mail-service-error'
+
+export class RegisterAndSendEmail implements UseCase {
+  private registerUser: RegisterUserOnMailingList
+  private sendEmail: SendEmail
+
+  constructor(registerUser: RegisterUserOnMailingList, sendEmail: SendEmail) {
+    this.registerUser = registerUser
+    this.sendEmail = sendEmail
+  }
+
+  async perform(
+    request: UserData
+  ): Promise<
+    Either<InvalidNameError | InvalidEmailError | MailServiceError, User>
+  > {
+    const userOrError: Either<InvalidNameError | InvalidEmailError, User> =
+      User.create(request)
+
+    if (userOrError.isLeft()) {
+      return left(userOrError.value)
+    }
+
+    const user: User = userOrError.value
+
+    await this.registerUser.perform(request)
+    const result = await this.sendEmail.perform(request)
+
+    if (result.isLeft()) {
+      return left(result.value)
+    }
+
+    return right(user)
+  }
+}
