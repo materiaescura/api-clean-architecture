0e9ebc1057c276dcec5bc14a9a93432b59dc1f4c
Diego Rabelo <rabelo.github@gmail.com>
2024-03-29 08:59:42 -0300

    refactor: change class name of register user controller for register and send email controller

--- src/main/adapters/express-route-adapter.ts
+++ src/main/adapters/express-route-adapter.ts
@@ -1,8 +1,8 @@
 import { HttpRequest } from '@/web-controllers/ports'
-import { RegisterUserController } from '@/web-controllers/register-and-send-email-controller'
+import { RegisterAndSendEmailController } from '@/web-controllers/register-and-send-email-controller'
 import { Request, Response } from 'express'
 
-export const adaptRoute = (controller: RegisterUserController) => {
+export const adaptRoute = (controller: RegisterAndSendEmailController) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = {
       body: req.body,
