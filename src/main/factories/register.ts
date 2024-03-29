44db2e2710e77f7833f5178ca009cc3cb5e2f72a
Diego Rabelo <rabelo.github@gmail.com>
2024-03-29 09:01:34 -0300

    refactor: use email config in send email use case

--- src/main/factories/register.ts
+++ src/main/factories/register.ts
@@ -1,14 +1,31 @@
 import { RegisterUserOnMailingList } from '@/usecases/register-user-on-mailing-list'
-import { RegisterUserController } from '@/web-controllers/register-and-send-email-controller'
+import { RegisterAndSendEmailController } from '@/web-controllers/register-and-send-email-controller'
 import { MongodbUserRepository } from '@/external/repositories/mongodb/mongodb-user-repository'
+import { NodemailerEmailService } from '@/external/mail-services/nodemailer-email-service'
+import { SendEmail } from '@/usecases/send-email/send-email'
+import { RegisterAndSendEmail } from '@/usecases/register-and-send-email/register-and-send-email'
+import { getEmailBody, getEmailConfig } from '../config/email'
 
-export const makeUserRegisterUserController = (): RegisterUserController => {
-  const inMemoryUserRepository = new MongodbUserRepository()
-  const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(
-    inMemoryUserRepository
-  )
-  const registerUserController = new RegisterUserController(
-    registerUserOnMailingListUseCase
-  )
-  return registerUserController
-}
+export const makeUserRegisterUserController =
+  (): RegisterAndSendEmailController => {
+    const mongoDbUserRepository = new MongodbUserRepository()
+    const registerUserOnMailingListUseCase = new RegisterUserOnMailingList(
+      mongoDbUserRepository
+    )
+    const emailService = new NodemailerEmailService()
+    const sendEmailUseCase = new SendEmail(
+      getEmailConfig(),
+      getEmailBody(),
+      emailService
+    )
+
+    const registerAndSendEmailUseCase = new RegisterAndSendEmail(
+      registerUserOnMailingListUseCase,
+      sendEmailUseCase
+    )
+    const controller = new RegisterAndSendEmailController(
+      registerAndSendEmailUseCase
+    )
+
+    return controller
+  }
