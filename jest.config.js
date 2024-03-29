795eb449bd82abec0ca51d9f1c095c1e292127b3
Diego Rabelo <rabelo.github@gmail.com>
2024-03-29 08:50:54 -0300

    chore: add dotenv in jest

--- jest.config.js
+++ jest.config.js
@@ -20,7 +20,8 @@ const config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
     '^@test/(.*)$': '<rootDir>/test/$1',
-  }
+  },
+  setupFiles: ['dotenv/config']
 }
 
 module.exports = config
